a60dd873096654171f7d50fdf6f06adf98cea751
Poonji <77487602+Poonji@users.noreply.github.com>
2022-04-25 14:37:28 +0530

    Create herons_formula.py

---
+++ herons_formula.py
@@ -0,0 +1,6 @@
+a = float(input('Enter the value of a: '))  
+b = float(input('Enter  the value of b: '))  
+c = float(input('Enter  the value of c: '))  
+s = (a + b + c) / 2  
+Area = (s(s-a)*(a-b)*(s-c))**0.5
+print('The area of the triangle is %f' %Area)
